30f6699315c1753b29ba9a4120b2ae87996ed30a
Hasan Lam <hcb.lam@gmail.com>
2021-09-14 04:37:10 +0000

    Added a tail function

---
+++ tail.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  actual === expected ?
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`) :
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+};
+
+
+const tail = function(array) {
+  let tailArray = [];
+  for (let i = 1; i < array.length; i++) {
+    tailArray.push(array[i]);
+  }
+  return tailArray;
+};
+
+
+const results = tail(['Hello', 'Lighthouse', 'Labs']);
+assertEqual(results.length, 2);
+assertEqual(results[0], 'Lighthouse');
+assertEqual(results[1], 'Labs');
+
