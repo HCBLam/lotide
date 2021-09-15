9001d8dc03827b6f75cd79effbec531c0f118a8b
Hasan Lam <hcb.lam@gmail.com>
2021-09-15 03:41:42 +0000

    Function to assert arrays are equal

--- eqArrays.js
+++ eqArrays.js
@@ -21,10 +21,10 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+eqArrays([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3], [3, 2, 1]); // => false
+eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
