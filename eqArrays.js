caa30bb81dfcd39729fee0cf47b3335cc51a8317
Hasan Lam <hcb.lam@gmail.com>
2021-09-15 03:14:14 +0000

    Function to compare arrays

---
+++ eqArrays.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  actual === expected ?
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`) :
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+};
+
+// Need to compare 2 arrays and see if they match perfectly, element for element.
+
+
+const eqArrays = function(array1, array2) {
+  // console.log('Array1: ' + array1);
+  // console.log('Array2: ' + array2);
+
+  if (array1.length !== array2.length) return false;
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
