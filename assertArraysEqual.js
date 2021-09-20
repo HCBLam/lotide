e63a8348502ea15e0af49255a62b5e56ff7aa0dc
Hasan Lam <hcb.lam@gmail.com>
2021-09-20 13:22:44 +0000

    Refactored to remove the test code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,20 +1,5 @@
-// This function compares 2 arrays to see if they are perfect matches, element for element.
-// It will take 2 arrays as arguments.
-// First it checks that both arrays have the same length; if not, arrays are not perfect matches.
-// Second, it loops through the *same* index for *both* arrays at the same time to check that iterated element.
-// If the 2 current elements are not strictly equal, then return false and stop the function.
-// If the 2 current elements are strictly equal, go onto the next index iteration and check.
-// If the loop is done and all elements match, then return true that the arrays are perfect matches.
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) return false;
-
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
 
 
 // This function asserts to see if 2 arrays are equal in value and type.
@@ -28,32 +13,5 @@ const assertArraysEqual = function(array1, array2) {
     console.log(`❌❌❌ Assertion Failed: ${array1} !== ${array2}`);
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
-assertArraysEqual(["a", "b", "c"], ["a", "b", "c"]); // => true
-assertArraysEqual(["x", "y", "z"], ["x", "y", 3]); // => false
 
-
-
-
-
-
-
-
-
-
-// eqArrays() and assertArraysEqual() could also have been combined into one function.
-
-const assertEqualArrays = function(array1, array2) {
-  if (array1.length !== array2.length) return false;
-
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      console.log(`❌❌❌ Assertion Failed: ${array1} !== ${array2}`);
-    }
-  }
-  console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
-};
-
-
-
+module.exports = assertArraysEqual;
