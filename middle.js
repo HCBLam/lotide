24c3fd745632cce6ca263afbb6ac746ef7724c40
Hasan Lam <hcb.lam@gmail.com>
2021-09-20 13:49:52 +0000

    Refactored to be modular and to remove test code

--- middle.js
+++ middle.js
@@ -1,21 +1,5 @@
-// This function compares 2 arrays to see if they are perfect matches, element for element.
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) return false;
-
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-// This function asserts to see if 2 arrays are equal in value and type.
-const assertArraysEqual = function(array1, array2) {
-  eqArrays(array1, array2) === true ?
-    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`) :
-    console.log(`❌❌❌ Assertion Failed: ${array1} !== ${array2}`);
-};
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = ('./assertArraysEqual');
 
 
 // Create a function that will take 1 array of any length.
@@ -53,21 +37,4 @@ const middle = function(array) {
 };
 
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-
-
-/*
-middle([1]);
-middle([1, 2]);
-
-middle([1, 2, 3]);
-middle([1, 2, 3, 4, 5]);
-
-middle([1, 2, 3, 4]);
-middle([1, 2, 3, 4, 5, 6]);
-*/
+module.exports = middle;
